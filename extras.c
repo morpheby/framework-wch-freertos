945cd3034023485f5ba5dbcc6c4fd34c0cfe790a
Ilya Mikhaltsou <morpheby@gmail.com>
2026-04-09 01:35:34 +0300

    Made it possible to see full heap

--- extras.c
+++ extras.c
@@ -6,6 +6,9 @@
 
 size_t xPortGetFreeHeapSize( void ) PRIVILEGED_FUNCTION {
     struct mallinfo mi = mallinfo();
-    return mi.fordblks;
-}
 
+    // Get total heap size from linker
+    extern size_t __heap_size;
+    
+    return (((size_t) &__heap_size) - mi.arena) + mi.fordblks;
+}
